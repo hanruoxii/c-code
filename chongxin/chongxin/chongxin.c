07e6fe5ab7a5c8face35af6c1833334b2f2da1e9
hanruoxii <3453654073@qq.com>
2024-09-01 16:58:49 +0800

    重新发送

---
+++ chongxin/chongxin/chongxin.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+int main()
+{
+	printf("ab\ncd");
+
+	printf("c:\\test\\test.c");
+
+	printf("(are you ok\?\?)");
+
+	printf("%c\n", '\'');
+
+
+	return 0;
+}
